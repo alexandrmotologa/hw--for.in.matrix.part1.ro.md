18b967d93f65b2efab6611450540e21e14b27924
Alexander Minaku <al.motologa@gmail.com>
2022-04-24 20:05:59 +0300

    HW

---
+++ app.py
@@ -0,0 +1,13 @@
+print("\n")
+
+for y in range(1, 11):
+    for x in range(1, 11):
+        if x == 1 or x == 10:
+            print("#", end=" ")
+        elif y == 1 or y == 10:
+            print("#", end=" ")
+        else:
+            print(".", end=" ")
+    print()
+
+print("\n")
